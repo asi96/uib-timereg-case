af2b643eeec9e0525b78c63bc4e3d0638270cebe
Anders Simonsen <andsim96@gmail.com>
2023-11-09 11:32:36 +0100

    PHP will now create the proper database schema if it does not exist

--- source/App.php
+++ source/App.php
@@ -1,13 +1,20 @@
 <?php
 
-    // Fetch the database file
-    $db = new PDO('sqlite:database.db');
+    main();
 
-    $statement = $db->query("SELECT * FROM Person");
+    function main() {
 
-    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+        // Fetch the database file
+        $db = new PDO('sqlite:database.db');
 
-    $jsondata = fetchFilteredServiceJSONData();
+        updateDatabase($db);
+
+        $statement = $db->query("SELECT * FROM Person");
+
+        $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        $jsondata = fetchFilteredServiceJSONData();
+    }
 
     function fetchFilteredServiceJSONData() {
 
@@ -31,4 +38,70 @@
         return $filtered_json;
     }
 
-?>
+    function updateDatabase($db) {
+
+        checkForProperExistingSchema($db);
+    }
+
+    function checkForProperExistingSchema($db) {
+
+        $checkPerson = databaseTableExists($db, 'Person');
+        $checkArbeidstid = databaseTableExists($db, 'Arbeidstid');
+        $checkTjeneste = databaseTableExists($db, 'Tjeneste');
+
+        if ($checkPerson == FALSE) {
+            $sql = "CREATE TABLE Person (
+                person_id	INTEGER NOT NULL,
+                navn	TEXT,
+                PRIMARY KEY('person_id')
+            )";
+            $db->query($sql);
+            $checkPerson == TRUE;
+        }
+
+        if ($checkTjeneste == FALSE) {
+            $sql = "CREATE TABLE Tjeneste (
+                tjeneste_id	INTEGER NOT NULL,
+                id	TEXT,
+                name	TEXT,
+                servicetype	TEXT,
+                supplier	TEXT,
+                owner	TEXT,
+                PRIMARY KEY('tjeneste_id')
+            )";
+            $db->query($sql);
+            $checkTjeneste == TRUE;
+        }
+
+        if ($checkArbeidstid == FALSE) {
+            $sql = "CREATE TABLE Arbeidstid (
+                arbeidstid_id	INTEGER NOT NULL,
+                ansatt_id	INTEGER NOT NULL,
+                tjeneste_id	INTEGER NOT NULL,
+                timer	INTEGER,
+                PRIMARY KEY('arbeidstid_id'),
+                FOREIGN KEY('ansatt_id') REFERENCES Person('person_id'),
+                FOREIGN KEY('tjeneste_id') REFERENCES Tjeneste('tjeneste_id')
+            )";
+            $db->query($sql);
+            $checkArbeidstid == TRUE;
+        }
+
+        echo "Database schema created successfully";
+    }
+
+    // Helper function to quickly check if table exists
+    function databaseTableExists($db, $table) {
+
+        try {
+            $result = $db->query("SELECT 1 FROM {$table} LIMIT 1");
+        } catch (Exception $e) {
+            return FALSE;
+        }
+
+        if ($result !== FALSE) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
